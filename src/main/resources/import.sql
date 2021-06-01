d58edc18d20646e81bccb67d3e278e3a67d95658
Guillaume Smet <guillaume.smet@gmail.com>
2021-06-01 19:25:05 +0200

    Ajout ressources statiques

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'AC/DC', 1973, null, true);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'Guns N Roses', 1985, null, true);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'Aerosmith', 1970, null, true);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'The Beatles', 1957, 1970, false);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'Led Zeppelin', 1968, 2007, false);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'The Rollings Stones', 1962, null, true);
+INSERT INTO band(id, name, creationYear, terminationYear, alive) VALUES (nextval('hibernate_sequence'), 'Nirvana', 1987, 1994, false);
